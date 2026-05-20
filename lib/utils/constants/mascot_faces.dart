2206f1947064f7f9f7ebbd7f8820aa71ec8f4e67
Jasmine Gargoles Magadan <jgmagadan@up.edu.ph>
2026-05-20 17:44:51 +0800

    refactor: moved static list of mascot faces to utlis folder

---
+++ lib/utils/constants/mascot_faces.dart
@@ -0,0 +1,10 @@
+class MascotAssets {
+  static const List<String> images = [
+    'assets/images/og.png',
+    'assets/images/dummy ahh.png',
+    'assets/images/shesh.png',
+    'assets/images/thinking 2.png',
+    'assets/images/thinking 1.png',
+    'assets/images/yum.png',
+  ];
+}
